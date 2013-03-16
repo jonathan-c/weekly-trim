0bccdeacaa443c2f75d80931b55c1d4621489a14
Jonathan Castillo <jonathan@ygm.com>
2013-03-16 15:31:26 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeeklyTrim::Application.config.secret_token = 'e67893c5db5d977a05c061b5b499fddff745a07ada542c4741de86366f9e3f02a9e70f960e7265e720cf38e9d92d959d8fb8fdf607568fb7f26a296352764981'
