cfbf6861fe554417a27bde16783c3a631a26fc74
Jonathan Castillo <jonathan@ygm.com>
2013-03-16 22:43:23 -0400

    fixed image url sizing

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,12 @@ class User < ActiveRecord::Base
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["info"]["name"]
-      user.image = auth["info"]["image"]
-      user.email = auth["info"]["email"]
+      if auth["provider"] == 'facebook'
+        user.email = auth["info"]["email"]
+        user.image = auth["info"]["image"].sub("square", "normal")
+      else
+        user.image = auth["info"]["image"].sub("_normal", "")
+      end
     end
   end
 end
