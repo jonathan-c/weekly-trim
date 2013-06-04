46b7b5404ffae74fdb969b6a6f54be45bd0339bd
Jonathan Castillo <joncastillo123@gmail.com>
2013-06-03 23:26:10 -0400

    modified show page and added signed_in? helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
+    
+    def signed_in?
+      current_user
+    end
+    helper_method :signed_in?
 end
