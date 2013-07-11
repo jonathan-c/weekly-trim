01f5aa4f9a78a3e430e16364e458cb10b34635a5
Jonathan Castillo <joncastillo123@gmail.com>
2013-07-11 18:39:18 -0400

    Added weekly trim email template

--- app/controllers/admin/mailer_controller.rb
+++ app/controllers/admin/mailer_controller.rb
@@ -3,11 +3,13 @@ class Admin::MailerController < Admin::ApplicationController
   def posts_email()
     @user = User.last
     @posts = Post.all
+    @group = Group.last
     render :file => 'posts_mailer/posts_email.html.erb', :posts_mailer => 'mailer', :layout=>false
   end
   
   def post_reminder_email()
     @user = User.last
+    @group = Group.first
     render :file => 'posts_mailer/post_reminder_email.html.erb', :posts_mailer => 'mailer', :layout=>false
   end
 
