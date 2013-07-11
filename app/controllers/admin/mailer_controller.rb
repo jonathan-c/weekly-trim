044eb909c4601d3d0eb3a022a90b6ec7ab97103a
Jonathan Castillo <joncastillo123@gmail.com>
2013-07-11 17:54:13 -0400

    set layout: false for viewing emails in browser

--- app/controllers/admin/mailer_controller.rb
+++ app/controllers/admin/mailer_controller.rb
@@ -3,12 +3,12 @@ class Admin::MailerController < Admin::ApplicationController
   def posts_email()
     @user = User.last
     @posts = Post.all
-    render :file => 'posts_mailer/posts_email.html.erb', :posts_mailer => 'mailer'
+    render :file => 'posts_mailer/posts_email.html.erb', :posts_mailer => 'mailer', :layout=>false
   end
   
   def post_reminder_email()
     @user = User.last
-    render :file => 'posts_mailer/post_reminder_email.html.erb', :posts_mailer => 'mailer'
+    render :file => 'posts_mailer/post_reminder_email.html.erb', :posts_mailer => 'mailer', :layout=>false
   end
 
 end
