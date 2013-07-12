bef85a90a3e0709207046a18fef1ab51e98d460f
Jonathan Castillo <joncastillo123@gmail.com>
2013-07-11 20:55:26 -0400

    only ask for email on first login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,17 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
-    session[:user_id] = user.id
-    session[:group_id], session[:auto_join] = params[:group_id], params[:auto_join]
-    redirect_to pages_ask_for_email_path
+    if User.find_by_provider_and_uid(auth["provider"], auth["uid"])
+      user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
+      session[:user_id] = user.id
+      session[:group_id], session[:auto_join] = params[:group_id], params[:auto_join]
+      redirect_to pages_dispatcher_path
+    else
+      user = User.create_with_omniauth(auth)
+      session[:user_id] = user.id
+      session[:group_id], session[:auto_join] = params[:group_id], params[:auto_join]
+      redirect_to pages_ask_for_email_path
+    end
   end
   
   def destroy
