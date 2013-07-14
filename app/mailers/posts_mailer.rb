688451fb632a4352282af27f5376877f9f7cd33d
Jonathan Castillo <joncastillo123@gmail.com>
2013-07-14 00:44:07 -0400

    set up mailer for prod

--- app/mailers/posts_mailer.rb
+++ app/mailers/posts_mailer.rb
@@ -6,14 +6,12 @@ class PostsMailer < ActionMailer::Base
     @posts = posts
     @group = group
     @posters_this_week = @group.posted_this_week
-    # mail(:to => user.email, :subject => "Weekly Trim")
-    mail(:to => "castillo@wow.com", :subject => "#{@group.name} Weekly Trim")
+    mail(:to => @user.email, :subject => "#{@group.name} Weekly Trim")
   end
   
   def post_reminder_email(user, group)
     @user = user
     @group = group
-    # mail(:to => user.email, :subject => "Weekly Trim")
-    mail(:to => "castillo@wow.com", :subject => "Don't forget to post something")
+    mail(:to => @user.email, :subject => "Don't forget to post something")
   end
 end
