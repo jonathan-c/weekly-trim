02848efb4733d6613c53111a48b4688fe13a059a
Jonathan Castillo <joncastillo123@gmail.com>
2013-07-13 21:16:41 -0400

    wrote copy for emails

--- app/mailers/posts_mailer.rb
+++ app/mailers/posts_mailer.rb
@@ -7,13 +7,13 @@ class PostsMailer < ActionMailer::Base
     @group = group
     @posters_this_week = @group.posted_this_week
     # mail(:to => user.email, :subject => "Weekly Trim")
-    mail(:to => "castillo@wow.com", :subject => "Weekly Trim")
+    mail(:to => "castillo@wow.com", :subject => "#{@group.name} Weekly Trim")
   end
   
   def post_reminder_email(user, group)
     @user = user
     @group = group
     # mail(:to => user.email, :subject => "Weekly Trim")
-    mail(:to => "castillo@wow.com", :subject => "Weekly Trim")
+    mail(:to => "castillo@wow.com", :subject => "Don't forget to post something")
   end
 end
