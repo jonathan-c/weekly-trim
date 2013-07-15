cc3e168773b59108ef9a79e33849173099ff5fb7
Jonathan Castillo <joncastillo123@gmail.com>
2013-07-14 22:46:40 -0400

    dont ask to confirm email if you already have account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     if user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
       session[:user_id] = user.id
       session[:group_id], session[:auto_join] = params[:group_id], params[:auto_join]
-      redirect_to pages_ask_for_email_path
+      redirect_to pages_dispatcher_path
     else
       user = User.create_with_omniauth(auth)
       session[:user_id] = user.id
